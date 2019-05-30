98aec044f3801d9a2fcc045e65de6a28e87e2dc5
Artom Lifshitz <alifshit@redhat.com>
2019-05-30 16:52:45 -0400

    Able to create Fedora VMs with static IPs

--- mkdevstack.sh
+++ mkdevstack.sh
@@ -1,55 +1,71 @@
 #!/bin/sh
 
-OPTIND=1
-
-default_user="artom"
-default_password="password"
-tmp_runscript="/tmp/mkdevstack.runscript.sh"
-
-while getopts "u:p:r:o:" opt; do
-    case "$opt" in
-    u)
-        user="$OPTARG"
-        ;;
-    p)
-        user_password="$OPTARG"
-        ;;
-    r)
-        root_password="$OPTARG"
-        ;;
-    o)
-        output_file="$OPTARG"
-        ;;
-    esac
-done
-
-if [ -z "$user" ]; then
-    user=$default_user
+if [ -z "$MKDS_USER" ]; then
+    echo "Please source mkdevstackrc before running this script."
+    exit
 fi
-if [ -z "$user_password" ]; then
-    user_password=$default_password
-fi
-if [ -z "$root_password" ]; then
-    root_password=$default_password
-fi
-if [ -z "$output_file" ]; then
-    echo "Missing -o <output file> argument."
+if [ $# -lt 1 ]; then
+    echo "Missing VM name."
     exit
 fi
 
-# TODO Find a way to not hardcode the pubkey
-cat <<EOF > $tmp_runscript
-password='`openssl passwd -6 $user_password`'
-useradd -G wheel -p "\$password" -m $user
-ssh_dir="/home/$user/.ssh"
-mkdir \$ssh_dir
-chown $user:$user \$ssh_dir
-chmod 700 \$ssh_dir
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWOmZ6xGuZlDxxpWWuQQZ2OzBP+kz6DPfwLFOcWtMm1El7W28rMOcEOE9htSRi3k22nBar/IyDuBxqPcAgt05AUB50cVRtj5/kj0ReEERIzOQKxH5VSATKiZ/ST8OTM9jj7ANvDM1LSZYWhzRCncg2bTLlHK7nKeRQfv3frRVG65dcB2Ks9gr40ht8lq9W5M35mnaMrNTPBX71oAOMQlFtBx0uqtSBPf8mxf5vz+6DmE5ptbEYK9xEW1FHj5f3h1RiPkRQs2LetH5u1RE3nZoCK/aqkf914IGIELb2M9F8Cd/ks0a/TwGDt+hXatybsmHIQNSqT27rQAbcUrlyYpav artom@zoe" > \$ssh_dir/authorized_keys
-EOF
+TMP_RUNSCRIPT="/tmp/mkdevstack.runscript.sh"
+TMP_FIRSTBOOT="/tmp/mkdevstack.firstboot.sh"
+MKDS_VM_NAME="$1"
+MKDS_DISK_IMAGE="$MKDS_VM_NAME.qcow2"
 
-virt-builder -o $output_file --size 50G --update --selinux-relabel \
-    --format qcow2 --root-password password:$root_password \
-    --install git,vim \
-    --run $tmp_runscript \
-    fedora-30
+cat <<RUNSCRIPT > $TMP_RUNSCRIPT
+password='`openssl passwd -6 $MKDS_USER_PASSWORD`'
+useradd -G wheel -p "\$password" -m $MKDS_USER
+SSH_DIR="/home/$MKDS_USER/.ssh"
+mkdir \$SSH_DIR
+chown $MKDS_USER:$MKDS_USER \$SSH_DIR
+chmod 700 \$SSH_DIR
+echo "$MKDS_SSH_PUBKEY" > \$SSH_DIR/authorized_keys
+cat <<EOF > /home/$MKDS_USER/.gitconfig
+[user]
+	email = $MKDS_GIT_EMAIL
+	name = $MKDS_GIT_NAME
+EOF
+echo "$MKDS_VIMRC" > /home/$MKDS_USER/.vimrc
+RUNSCRIPT
+
+# network-scripts is deprecated and even static IPs in ifcfg-style config files
+# are handled by NetworkManager.
+cat <<FIRSTBOOT > $TMP_FIRSTBOOT
+systemctl disable firewalld.service
+systemctl stop firewalld.service
+nmcli connection show
+DEVICE=\`nmcli --fields DEVICE connection show --active | tail -n1 | xargs\`
+echo "Device: \$DEVICE"
+HWADDR=\`ifconfig \$DEVICE | grep ether | awk '{print \$2}'\`
+cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-\$DEVICE
+NAME="\$DEVICE"
+DEVICE="\$DEVICE"
+ONBOOT="yes"
+TYPE="Ethernet"
+HWADDR="\$HWADDR"
+BOOTPROTO="none"
+IPADDR="$MKDS_STATIC_IP"
+NETMASK="$MKDS_NETMASK"
+GATEWAY="$MKDS_GATEWAY"
+DNS1="$MKDS_NAMESERVER"
+EOF
+reboot
+FIRSTBOOT
+
+virt-builder fedora-30 \
+    --size 50G --update --selinux-relabel --format qcow2 \
+    --install git,vim,net-tools,bash-completion \
+    --output $MKDS_DISK_IMAGE \
+    --root-password password:$MKDS_ROOT_PASSWORD \
+    --run $TMP_RUNSCRIPT \
+    --hostname $MKDS_VM_NAME \
+    --firstboot $TMP_FIRSTBOOT
+sudo mv $MKDS_DISK_IMAGE /var/lib/libvirt/images
+sudo virt-install --import --nographics \
+    --vcpus $MKDS_CPUS \
+    --ram $MKDS_RAM \
+    -w network=$MKDS_VIRT_NETWORK \
+    --disk /var/lib/libvirt/images/$MKDS_DISK_IMAGE \
+    --name $MKDS_VM_NAME 
